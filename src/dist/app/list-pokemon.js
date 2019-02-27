e35577779f2a9315505e6ab066c367acec272ea8
youssef2013 <benkhallouf.youssef@gmail.com>
2019-02-27 23:04:37 +0100

    remove dist folder

--- src/dist/app/list-pokemon.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=list-pokemon.js.map
--- src/dist/app/pokemons/pokemon.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Pokemon = (function () {
-    function Pokemon() {
-    }
-    return Pokemon;
-}());
-exports.Pokemon = Pokemon;
-//# sourceMappingURL=pokemon.js.map
